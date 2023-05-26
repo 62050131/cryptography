ca562bfc42be67e4ea7af03eb6be62ac652618e8
Kaewthip <105012524+62050131@users.noreply.github.com>
2023-05-26 20:44:37 +0700

    Add files via upload

--- El_sender/Send.java
+++ El_sender/Send.java
@@ -1,5 +1,7 @@
 package El_sender;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.*;
 
 
@@ -8,8 +10,9 @@ public class Send {
         Scanner sc=new Scanner(System.in);
         ConvertFile conv=new ConvertFile();
         System.out.print("Enter n for length prime ==> ");
-        long n=sc.nextLong();
+        int n=sc.nextInt();
         // String inputfile="D:/Crypto/code/input.txt";
+        //===============================input data and convert to plaintext binary================================================
         System.out.print("Enter 1 for message 2 for file ==> ");
         int choose=sc.nextInt();
         sc.nextLine();
@@ -29,8 +32,34 @@ public class Send {
         System.out.println(binarytext);
         System.out.println("Data length = "+binarytext.length());
         
+        // ==============================generate key=====================================================
         
+        BigInteger minP=Power(n-1);
+        BigInteger maxP=Power(n);
+        System.out.println("minP is "+minP);
+        System.out.println("maxP is "+maxP);
+        BigInteger p = RandomNumber(minP,maxP);
+        System.out.println("Random Number: " + p);
         
+    }
+    public static BigInteger Power(int num){
+        int base = 2, exponent = num;
+        BigInteger result = BigInteger.valueOf(1);
+
+        for (; exponent != 0; --exponent) {
+            result =result.multiply(BigInteger.valueOf(base));
+        }
+        return result;
+    }
+
+    public static BigInteger RandomNumber(BigInteger min,BigInteger max){
+        SecureRandom random = new SecureRandom();
+        BigInteger p = new BigInteger(max.bitLength(), random);
+
+        while (p.compareTo(min) < 0 || p.compareTo(max) > 0) {
+            p = new BigInteger(max.bitLength(), random);
+        }
+        return p;
     }
     
 }
